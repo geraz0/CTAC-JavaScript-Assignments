9149657273daba4f23d7fd48450974324fae43fc
geraz0 <gabbi.erazo1@gmail.com>
2024-06-24 16:26:49 -0400

    functio to add an item to cart

--- cartApp.js
+++ cartApp.js
@@ -2,4 +2,10 @@ function initializeCart() {
     if (localStorage.getItem('cart') === null) {
         localStorage.setItem('cart', JSON.stringify([]));
     }
-    
+}
+
+function addToCart(product) {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    cart.push(product);
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
